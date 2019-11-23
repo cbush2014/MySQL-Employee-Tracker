5963a621e062aa8906a2191bf2091ef1b93a754b
cbush2019 <cbush5400@gmail.com>
2019-11-23 16:22:33 -0500

    add eetracker.sql to create db and tables

---
+++ eetracker.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS ee_tracker;
+
+CREATE DATABASE ee_tracker;
+
+USE ee_tracker;
+CREATE TABLE departments (
+    department_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+    name VARCHAR(30)
+    );
+
+CREATE TABLE roles (
+    role_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+    title VARCHAR(30),
+    salary DEC(10,2),
+    department_id INTEGER,
+    CONSTRAINT departments_department_id_fk FOREIGN KEY (department_id)
+    REFERENCES departments (department_id)
+    );
+
+CREATE TABLE employees (
+    ee_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INTEGER NOT NULL,
+    CONSTRAINT roles_role_id_fk FOREIGN KEY (role_id) 
+    REFERENCES roles (role_id),
+    manager_id INTEGER,
+    CONSTRAINT employees_ee_id_fk FOREIGN KEY (ee_id) 
+    REFERENCES employees (ee_id)
+    );
+
+
+
+
